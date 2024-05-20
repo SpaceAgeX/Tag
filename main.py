6c76cd4f5acd7926457f6e8a1f716f1fb8ec9188
SpaceAgeX <aaryeh27@palmertrinity.org>
2024-05-20 14:00:13 -0400

    Finished Map Data With Images

--- main.py
+++ main.py
@@ -9,35 +9,122 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 clock = pygame.time.Clock()
-running = True
 
-surf = pygame.Surface((50, 50))
-surf.fill("green")
 
+player1_surf = pygame.Surface((50, 50))
+player1_surf.fill("indigo")
+player2_surf = pygame.Surface((50, 50))
+player2_surf.fill("indigo")
+
+tileImages = [None]
+for x in range(0, 6):
+    tileImages.append(pygame.image.load('Assets/GrassTiles/GrassTile'+str(x+1)+'.png').convert_alpha())
 
 platforms = []
 
-for i in range(10):
-    platform = Platform.from_rect(screen, pygame.Rect(random.randrange(0, WIDTH), random.randrange(0, HEIGHT), random.randrange(40, 100), 30), "blue")
-    platforms.append(platform)
+map_data = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,2,4,0,0,0,0,0,0,0,0,0],
+            [0,2,4,0,0,0,0,0,0,0,0,0,0,0,0,2,3,4,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,2,3,3,3,3,4,0,0,0,0,0,0,0],
+            [0,0,0,0,0,2,4,0,0,0,0,0,0,1,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0],
+            [0,0,2,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,1,0,0,0,2,3,3,4,0,0,0,2,4,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4]]
+
+for row, i in enumerate(map_data):
+    for tile, j in enumerate(i):
+        if j > 0:
+            platform = Platform(screen, pygame.transform.scale_by(tileImages[j], 2), (tile*64, (row*64)+1))
+            platforms.append(platform)
 
 
-player = Player(screen, surf, platforms)
+def set_all_players(players):
+    for player in players:
+        player.set_players(players)
+    
+    chosen_index = random.randrange(0, len(players))
+    players[chosen_index].set_it(True)
+    print(chosen_index)
 
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+class TagPlayer(Player):
+    def __init__(self, screen, surf, platforms):
+        super().__init__(screen, surf, platforms)
+        self.opponents = None
+        self.it = False
+        self.id = random.random()
+        self.collided = False
+    
 
-    screen.fill("black")
+    def set_it(self, value):
+        if value == True:
+            self.it = True
+            self.surf.fill("red")
+        else:
+            self.it = False
+            self.surf.fill("indigo")
+    
 
-    player.update()
-    for platform in platforms:
-        platform.draw()
+    def set_players(self, players):
+        self.opponents = list(filter(lambda p: p.id != self.id, players))
+    
 
-    pygame.display.update()
-    clock.tick(60)
+    def update(self):
+        super().update()
+        mouse_clicked = pygame.mouse.get_pressed()[0]
+        opponent_rects = list(map(lambda o: o.rect, self.opponents))
+
+        if self.rect.collideobjects(opponent_rects):
+            if self.collided == False:
+                self.collided = True
+
+                if self.it:
+                    self.set_it(False)
+                else:
+                    self.set_it(True)
+        else:
+            self.collided = False
+
+        # Debugging
+        if mouse_clicked:
+            print(f"It ({self.id}): ", self.it)
+        
+
+def main():
+    player1 = TagPlayer(screen, player1_surf, platforms)
+    player1.rect.topleft = ((WIDTH/2)- 128, (HEIGHT/2)-128)
+    player1.controls = "wasd"
+
+    player2 = TagPlayer(screen, player2_surf, platforms)
+    player2.rect.topleft = ((WIDTH/2 + 128), (HEIGHT/2)-128)
+    player2.controls = "arrow_keys"
+
+    players = [player1, player2]
+    set_all_players(players)
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("deepskyblue3")
+
+        player1.update()
+        player2.update()
+
+        for platform in platforms:
+            platform.draw()
+
+        pygame.display.update()
+        clock.tick(60)
 
 
-pygame.quit()
+    pygame.quit()
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 class Player:
     def __init__(self, rendering_screen, player_surf, platforms):
@@ -7,11 +8,21 @@ class Player:
         self.screen = rendering_screen
         self.platforms = platforms
 
-        self.speed = 3
+        # Running Variables
+        self.acceleration = 0.5
+        self.accelerating = True
+        self.running = False
+        self.speed = 7
+        self.run_progress = 0
+        self.direction = 0
         
+        # Jumping Variables
+        self.gravity_strength = 0.25
         self.jumping = False
         self.jump_power = 10
         self.jump_progress = 0
+
+        self.controls = "wasd" # "arrow_keys" or "wasd"
 
 
     def draw(self):
@@ -21,10 +32,10 @@ class Player:
     def is_on_floor(self):
         rects = list(map(lambda p: p.get_top(), self.platforms)) # Rects of all Platforms in Environment
 
-        if rect := self.rect.collideobjects(rects):
-            #self.rect.bottom = rect.top+1
+        if self.rect.collideobjects(rects):
             return True
         elif self.rect.bottom >= self.screen.get_height():
+            self.rect.bottom = self.screen.get_height()
             return True
         else:
             return False
@@ -32,81 +43,133 @@ class Player:
 
     def update_jump(self):
         self.rect.move_ip(0, -self.jump_power+self.jump_progress)
-        self.jump_progress += 0.25
+        self.jump_progress += self.gravity_strength
 
         if self.is_on_floor():
             self.jumping = False
             self.jump_progress = 0
     
 
+    def update_run(self):
+        self.rect.move_ip(self.direction*self.run_progress, 0)
+
+        if self.accelerating: self.run_progress += self.acceleration
+        else: self.run_progress -= self.acceleration
+
+        if self.run_progress > self.speed:
+            self.run_progress = self.speed
+        
+        if self.run_progress < 0:
+            self.run_progress = 0
+            self.running = False
+    
+
     def update(self):
         self.draw()
         keys = pygame.key.get_pressed()
+
+        key_left = keys[pygame.K_a] if self.controls == "wasd" else keys[pygame.K_LEFT]
+        key_right = keys[pygame.K_d] if self.controls == "wasd" else keys[pygame.K_RIGHT]
+        key_up = keys[pygame.K_w] if self.controls == "wasd" else keys[pygame.K_UP]
+
         mouse_clicked = pygame.mouse.get_pressed()[0]
+    
+        if self.jumping:
+            self.update_jump()
+        
+        if self.running:
+            self.update_run()
 
         # Debugging
-        if mouse_clicked:
-            print("Jumping: ", self.jumping)
-            print("Jump Progress: ", self.jump_progress)
-            print("On Floor: ", self.is_on_floor())
+        # if mouse_clicked:
+        #     print("Jumping: ", self.jumping)
+        #     print("Jump Progress: ", self.jump_progress)
+        #     print("On Floor: ", self.is_on_floor())
 
         # Gravity
         if self.jumping == False and self.is_on_floor() == False:
             self.jump_progress = self.jump_power # Only Do the Descending Portion of the Jump
             self.jumping = True
 
-        # Jumping
-        if keys[pygame.K_SPACE] or keys[pygame.K_w]:
-            self.jumping = True
-
-        if self.jumping:
-            self.update_jump()
         
         # Player Movement
-        if keys[pygame.K_a] == True:
-            self.rect.move_ip(-self.speed, 0)
-        elif keys[pygame.K_d] == True:
-            self.rect.move_ip(self.speed, 0)
+        if key_left == True:
+            if self.direction == 1: self.run_progress = 0
+            self.running = True
+            self.accelerating = True
+            self.direction = -1
+            
+        elif key_right == True:
+            if self.direction == -1: self.run_progress = 0
+            self.running = True
+            self.accelerating = True
+            self.direction = 1
+        else:
+            self.accelerating = False
+        if key_up:
+            self.jumping = True
         
-        # Bottom Collision
-        rects = list(map(lambda p: p.get_bottom(), self.platforms))
-        if self.rect.collideobjects(rects):
+
+        # Platform Collision
+        top_rects = list(map(lambda p: p.get_top(), self.platforms))
+        bottom_rects = list(map(lambda p: p.get_bottom(), self.platforms))
+        left_rects = list(map(lambda p: p.get_left(), self.platforms))
+        right_rects = list(map(lambda p: p.get_right(), self.platforms))
+
+        if rect := self.rect.collideobjects(left_rects):
+            if abs(self.rect.right - rect.left) < self.speed+1:
+                self.rect.right = rect.left-1
+
+        if rect := self.rect.collideobjects(right_rects):
+            if abs(self.rect.left - rect.right) < self.speed+1:
+                self.rect.left = rect.right+1
+
+        if rect := self.rect.collideobjects(top_rects):
+            if abs(self.rect.bottom - rect.top) < self.speed+1:
+                self.rect.bottom = rect.top+1
+
+        if rect := self.rect.collideobjects(bottom_rects):
+            self.rect.top = rect.bottom+1
             self.jump_progress = self.jump_power
             self.jumping = True
-
+        
 
 
 class Platform:
-    def __init__(self, screen, surf):
+    def __init__(self, screen, surf, pos):
         self.surf = surf
-        self.rect = surf.get_rect()
+        self.rect = pygame.Rect(pos[0], pos[1], surf.get_width(), surf.get_height()-16)
         self.screen = screen
+        self.pos = (pos[0], pos[1]- 5)
+        self.direction = 1
     
     
-    @staticmethod
-    def from_rect(screen, rect: pygame.Rect, color):
-        surf = pygame.Surface((rect.w, rect.h))
-        platform = Platform(screen, surf)
-        platform.surf.fill(color)
-        platform.rect.x = rect.x
-        platform.rect.y = rect.y
-        return platform
+    
 
 
     def get_top(self):
-        top_rect = pygame.Rect(self.rect.x, self.rect.y, self.rect.w, 2)
+        top_rect = pygame.Rect(self.rect.x+2, self.rect.y, self.rect.w-4, 2)
         return top_rect
     
 
     def get_bottom(self):
-        rect = pygame.Rect(self.rect.x, self.rect.y+self.rect.h-2, self.rect.w, 2)
+        rect = pygame.Rect(self.rect.x+2, self.rect.y+self.rect.h-2, self.rect.w-4, 2)
+        return rect
+
+    def get_left(self):
+        rect = pygame.Rect(self.rect.x, self.rect.y+2, 2, self.rect.h-4)
+        return rect
+
+
+    def get_right(self):
+        rect = pygame.Rect(self.rect.x+self.rect.w-2, self.rect.y+2, 2, self.rect.h-4)
         return rect
 
 
     def draw(self):
-        self.screen.blit(self.surf, self.rect)
+        self.screen.blit(self.surf, self.pos)
     
 
-    def update():
-        pass
+    def update(self):
+        self.rect.move_ip(self.direction, 0)
 
